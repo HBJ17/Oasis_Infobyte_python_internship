1a1527c62183fa0a2a0a14dc2f19186287bbc399
HBJ17 <hbj17672007@gmail.com>
2026-06-22 18:56:35 +0530

    planning Structure of the program

---
+++ Task_1/voice_assisstant.py
@@ -0,0 +1,18 @@
+import datetime
+import pyttsx3
+import speech_recognition as r
+import webbrowser
+
+def speak():
+    
+def listen():
+
+def tell_time():
+
+def tell_date():
+
+def search_web():
+
+def process_commands():
+
+def main():
